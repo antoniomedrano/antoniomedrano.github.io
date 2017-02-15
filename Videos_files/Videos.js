420664c1ec646c6152a8c98dd0d6ab4f44a3f4a2
Antonio Medrano <tonedabass@gmail.com>
2017-02-15 13:20:53 -0800

    updated method of linking YouTube vids

--- Videos_files/Videos.js
+++ Videos_files/Videos.js
@@ -1,11 +1,9 @@
 // Created by iWeb 3.0.4 local-build-20170215
 
-setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,353),url:'Videos_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'Videos_files/stroke_1.png'},{rect:new IWRect(1,-1,423,2),url:'Videos_files/stroke_2.png'},{rect:new IWRect(424,-1,2,2),url:'Videos_files/stroke_3.png'},{rect:new IWRect(424,1,2,353),url:'Videos_files/stroke_4.png'},{rect:new IWRect(424,354,2,2),url:'Videos_files/stroke_5.png'},{rect:new IWRect(1,354,423,2),url:'Videos_files/stroke_6.png'},{rect:new IWRect(-1,354,2,2),url:'Videos_files/stroke_7.png'}],new IWSize(425,355))});registry.applyEffects();}
-function hostedOnDM()
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Videos_files/VideosMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
