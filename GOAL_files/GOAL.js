469618e1511f28b104439324023f0de811d21f41
Antonio Medrano <tonedabass@gmail.com>
2020-10-17 20:05:03 -0500

    add GOAL webpage

---
+++ GOAL_files/GOAL.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20201017
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,298),url:'GOAL_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'GOAL_files/stroke_1.png'},{rect:new IWRect(1,-1,212,2),url:'GOAL_files/stroke_2.png'},{rect:new IWRect(213,-1,3,2),url:'GOAL_files/stroke_3.png'},{rect:new IWRect(213,1,3,298),url:'GOAL_files/stroke_4.png'},{rect:new IWRect(213,299,3,2),url:'GOAL_files/stroke_5.png'},{rect:new IWRect(1,299,212,2),url:'GOAL_files/stroke_6.png'},{rect:new IWRect(-1,299,2,2),url:'GOAL_files/stroke_7.png'}],new IWSize(214,300)),shadow_0:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.500000}),shadow_1:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.500000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('GOAL_files/GOALMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
--- Home_files/Home.js
+++ Home_files/Home.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20200707
+// Created by iWeb 3.0.4 local-build-20201017
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
