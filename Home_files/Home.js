383952b26bc8d6cff97e561b4ce79547ef12af56
Antonio Medrano <tonedabass@gmail.com>
2021-03-31 11:13:51 -0500

    add new publication

--- Home_files/Home.js
+++ Home_files/Home.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20201121
+// Created by iWeb 3.0.4 local-build-20210331
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
--- Videos_files/Videos.js
+++ Videos_files/Videos.js
@@ -1,9 +1,9 @@
-// Created by iWeb 3.0.4 local-build-20191116
+// Created by iWeb 3.0.4 local-build-20210331
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Videos_files/VideosMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
