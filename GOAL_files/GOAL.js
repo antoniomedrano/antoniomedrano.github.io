98080ea3a9f2479ed50ef5ec47591b1005d4bb78
Antonio Medrano <tonedabass@gmail.com>
2021-04-03 20:56:36 -0500

    add AI2ES link to Marina's bio

--- GOAL_files/GOAL.js
+++ GOAL_files/GOAL.js
@@ -1,11 +1,11 @@
-// Created by iWeb 3.0.4 local-build-20210331
+// Created by iWeb 3.0.4 local-build-20210403
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_2:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.500000}),stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,277),url:'GOAL_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'GOAL_files/stroke_1.png'},{rect:new IWRect(1,-1,198,2),url:'GOAL_files/stroke_2.png'},{rect:new IWRect(199,-1,2,2),url:'GOAL_files/stroke_3.png'},{rect:new IWRect(199,1,2,277),url:'GOAL_files/stroke_4.png'},{rect:new IWRect(199,278,2,3),url:'GOAL_files/stroke_5.png'},{rect:new IWRect(1,278,198,3),url:'GOAL_files/stroke_6.png'},{rect:new IWRect(-1,278,2,3),url:'GOAL_files/stroke_7.png'}],new IWSize(200,280)),shadow_1:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.500000}),shadow_0:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.500000})});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,277),url:'GOAL_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'GOAL_files/stroke_1.png'},{rect:new IWRect(1,-1,198,2),url:'GOAL_files/stroke_2.png'},{rect:new IWRect(199,-1,2,2),url:'GOAL_files/stroke_3.png'},{rect:new IWRect(199,1,2,277),url:'GOAL_files/stroke_4.png'},{rect:new IWRect(199,278,2,3),url:'GOAL_files/stroke_5.png'},{rect:new IWRect(1,278,198,3),url:'GOAL_files/stroke_6.png'},{rect:new IWRect(-1,278,2,3),url:'GOAL_files/stroke_7.png'}],new IWSize(200,280)),shadow_2:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.500000}),shadow_0:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.500000}),shadow_1:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.500000})});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('GOAL_files/GOALMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
