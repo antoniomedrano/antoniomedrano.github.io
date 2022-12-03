d2c2bf1f07745b9093301fceea9286725d91e9c8
antoniomedrano <tonedabass@gmail.com>
2022-12-03 13:53:26 -0600

    new GOAL news and pubs page

---
+++ GOAL_News_&_Pubs_files/GOAL_News_&_Pubs.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20221203
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('GOAL_News_&_Pubs_files/GOAL_News_&_PubsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
