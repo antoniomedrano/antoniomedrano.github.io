a5b90587e005ea4ece40f445c79ee0ae61a2d729
antoniomedrano <tonedabass@gmail.com>
2024-05-13 13:10:35 -0500

    a few CV and GOAL updates

--- Curriculum_Vitae_files/Curriculum_Vitae.js
+++ Curriculum_Vitae_files/Curriculum_Vitae.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20240410
+// Created by iWeb 3.0.4 local-build-20240513
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
--- GOAL_News_&_Pubs_files/GOAL_News_&_Pubs.js
+++ GOAL_News_&_Pubs_files/GOAL_News_&_Pubs.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20240411
+// Created by iWeb 3.0.4 local-build-20240513
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
