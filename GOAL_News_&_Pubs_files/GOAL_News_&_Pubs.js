0dff3564032c64ce1a44453e86d627b1ed10b713
antoniomedrano <tonedabass@gmail.com>
2024-06-27 19:11:32 -0500

    GOAL updates

--- GOAL_News_&_Pubs_files/GOAL_News_&_Pubs.js
+++ GOAL_News_&_Pubs_files/GOAL_News_&_Pubs.js
@@ -1,9 +1,9 @@
-// Created by iWeb 3.0.4 local-build-20240513
+// Created by iWeb 3.0.4 local-build-20240627
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('GOAL_News_&_Pubs_files/GOAL_News_&_PubsMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
